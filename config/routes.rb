56271e2856c70ef88e068ab006893e550961daa8
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-20 21:08:04 +0500

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :users, only: %i[index show] do
+    resources :articles
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: 'Hope', email: 'hope@jons.com', password: '123456', password_confirmation: '123456')
-User.create(name: 'Amy', email: 'amy@jons.com', password: '123456', password_confirmation: '123456')
-User.create(name: 'Jimy', email: 'hpes@jons.com', password: '123456', password_confirmation: '123456')
+hope = User.create(name: 'Hope', email: 'hope@jons.com', password: '123456', password_confirmation: '123456')
+hope.articles.create(title: 'Test', text: 'test test test')
+hope.articles.create(title: 'Test 1', text: 'test test test 2')
+hope.articles.create(title: 'Test 2', text: 'test test test 3')
+hope.articles.create(title: 'Test 3', text: 'test test test 4')
+hope.articles.create(title: 'Test 6', text: 'test test test 5')
