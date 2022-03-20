c182e8c27ab681c6c93daca7cb79cf23cc454e02
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-20 21:20:10 +0500

    Add create action to user ctr

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[index show] do
+  resources :users, only: %i[show create] do
     resources :articles
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
